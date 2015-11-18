01e9f6c708c1227eff35dfa0f8cd25e81b86d588
Jakub Warmuz <jakub@warmuz.org>
2015-11-18 22:07:34 +0000

    Travis experiment: first simple test

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,12 +1,33 @@
 #!/bin/sh -xe
 
 integration_install() {
-  ./venv.sh
+  ./venv.sh &
+
+  # `/...` avoids `no buildable Go source files` errors, for more info
+  # see `go help packages`
+  go get -d github.com/letsencrypt/boulder/... &
+  (wget https://github.com/jsha/boulder-tools/raw/master/goose.gz && \
+   mkdir $GOPATH/bin && \
+   zcat goose.gz > $GOPATH/bin/goose && \
+   chmod +x $GOPATH/bin/goose) &
+  # listenbuddy is needed for ./start.py
+  go get github.com/jsha/listenbuddy &
+
+  wait
+
+  cd $GOPATH/src/github.com/letsencrypt/boulder
+  ./start.py &
+  cd -
+
+  mkdir public_html
+  python -m SimpleHTTPServer 5002 &
 }
 
 integration_script() {
   . venv/bin/activate
   pip -V
+
+  simp_le -f key.pem -f cert.pem -f fullchain.pem -d le.wtf:public_html
 }
 
 case $1 in
