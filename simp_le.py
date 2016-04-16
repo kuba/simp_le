cfe6860c128a3b3e326a2772ba6403774abf593b
Jakub Warmuz <jakub@warmuz.org>
2016-04-16 18:09:40 +0000

    Change ToS SHA256 to match testing Boulder.
    https://github.com/letsencrypt/boulder/pull/1742

--- simp_le.py
+++ simp_le.py
@@ -1464,8 +1464,8 @@ class IntegrationTests(unittest.TestCase):
     # this is a test suite | pylint: disable=missing-docstring
 
     SERVER = 'http://localhost:4000/directory'
-    TOS_SHA256 = ('3ae9d8149e59b8845069552fdae761c3'
-                  'a042fc5ede1fcdf8f37f7aa4707c4d6e')
+    TOS_SHA256 = ('b16e15764b8bc06c5c3f9f19bc8b99fa'
+                  '48e7894aa5a6ccdad65da49bbf564793')
     PORT = 5002
 
     @classmethod
